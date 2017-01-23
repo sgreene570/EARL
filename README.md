20c03b3c6ce501086398f38cb57d80ed28835026
Stephen Greene <sgreene@sgreene>
2017-01-22 21:38:15 -0500

    working version

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # EARL
 EMail Alias Reversal Lookup
+<br>
+A simple python script to parse alias files and output corresponding names or email addresses.  
+<br>
+Could easily be pipelined into other services.
+
--- earl.py
+++ earl.py
@@ -11,7 +11,7 @@ import argparse
 def main():
     parser = argparse.ArgumentParser(description='Email Alias Lookup')
     parser.add_argument('file', type=str, nargs='+', help="Alias file name(s)")
-    parser.add_argument('--alias', type=str, nargs='+', help="Alias to be found")
+    parser.add_argument('--alias', type=str, nargs='+', help="Alias to find")
     args = parser.parse_args()
     alias_dict = {}
 
@@ -20,18 +20,19 @@ def main():
         for line in alias_list:
             if "#" == line[0] or ":" not in line:
                 continue
-            alias = line[:line.index(":")].strip(" \n")
-            name = line[line.index(":") + 1:]
-            print(name)
+            alias = line[:line.index(":")].strip()
+            name = line[line.index(":") + 1:].strip()
             if alias in args.alias:
                 if alias in alias_dict.keys():
                     alias_set = alias_dict[alias]
-                    alias_dict[alias] = alias_set.add(name)
+                    alias_set.add(name)
+                    alias_dict[alias] = alias_set
                 else:
                     alias_dict[alias] = {name}
 
     for alias in args.alias:
-        print(alias + ":" + str(alias_dict[alias]))
+        print(alias + ": " + 
+            str(alias_dict[alias]).replace("'", "").strip("{}'"))
 
 
 if __name__ =="__main__":
